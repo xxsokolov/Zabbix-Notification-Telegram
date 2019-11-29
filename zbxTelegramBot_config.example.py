43d9c36b481021fd747451618fce1942d9f3f9f2
xxsokolov <xx.sokolov@gmail.com>
2019-11-29 22:09:59 +0300

    Рефактор:  - Заполнил ексемл конфиг бота

---
+++ zbxTelegramBot_config.example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+########################
+#    Sokolov Dmitry    #
+# xx.sokolov@gmail.com #
+#  https://t.me/ZbxNTg #
+########################
+tg_proxy = True
+tg_proxy_server = {'https': 'socks5://username:password@domen:port'}
+tg_token = '123123123123:ADDDD_er9beG-fGx33ktYqFkUpAdUtWe2s'
